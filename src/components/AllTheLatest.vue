1c20f4bc07280822a0b8161a6f00ea0f2d69975c
Alexis <alxssnts@gmail.com>
2024-03-12 23:19:42 +0800

    Created second component

---
+++ src/components/AllTheLatest.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+    import Card from './Card.vue';
+    import data from '@/data'
+</script>
+
+<template>
+    <div>
+        <h2 class="text-4xl uppercase text-center">All the latest from AEG</h2>
+        <div class="grid grid-cols-1 lg:grid-cols-3 gap-10 mx-auto mt-16">
+            <template v-for="item in data">
+                <Card :item="item" />
+            </template>
+        </div>
+    </div>
+</template>
---
+++ src/components/Card.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+interface CardProps {
+    id: number
+    image: string
+    title: string
+    description: string
+    url: string
+}
+
+defineProps<{
+    item: CardProps
+}>()
+</script>
+
+<template>
+    <a :href="item.url" class="card-wrapper">
+        <img class="w-full border-b-4 border-red-500 mb-4 transition duration-300 ease-in-out" :src="item.image" alt="Card Image">
+        <h3 class="text-lg font-bold mb-2">{{ item.title }}</h3>
+        <p class="text-md font-light mb-2 md:mb-6">{{ item.description }}</p>
+        <a :href="item.url" class="border-b-2 border-red-500 uppercase">Read more</a>
+    </a>
+</template>
+
+<style scoped>
+    .card-wrapper:hover {
+        img {
+            transform: scale(105%);
+        }
+
+        cursor: pointer;
+    }
+</style>
